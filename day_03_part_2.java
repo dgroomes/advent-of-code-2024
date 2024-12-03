2d7d580ac33ef50aba33f8673744e228a3e5026d
David Groomes <dwgroomes@gmail.com>
2024-12-03 00:13:02 -0600

    fit on screen

--- day_03_part_2.java
+++ day_03_part_2.java
@@ -5,34 +5,34 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.regex.Pattern;
 
+sealed interface Match {
+    int idx();
+
+    record Mul(int idx, int product) implements Match {}
+
+    record Do(int idx) implements Match {}
+
+    record Dont(int idx) implements Match {}
+}
+
 void main() throws IOException {
     var memory = memory();
     var matches = new ArrayList<Match>();
 
     // Find all matches across 'mul', 'do' and "don't" instructions and then sort it.
-    {
-        var matcher = Pattern.compile("mul\\((\\d{1,3}),(\\d{1,3})\\)").matcher(memory);
-        while (matcher.find()) {
-            var op1 = Integer.valueOf(matcher.group(1));
-            var op2 = Integer.valueOf(matcher.group(2));
-            var product = op1 * op2;
-            matches.add(new Match.Mul(matcher.start(), product));
-        }
+    var mulMatcher = Pattern.compile("mul\\((\\d{1,3}),(\\d{1,3})\\)").matcher(memory);
+    while (mulMatcher.find()) {
+        var op1 = Integer.valueOf(mulMatcher.group(1));
+        var op2 = Integer.valueOf(mulMatcher.group(2));
+        var product = op1 * op2;
+        matches.add(new Match.Mul(mulMatcher.start(), product));
     }
 
-    {
-        var matcher = Pattern.compile("do\\(\\)").matcher(memory);
-        while (matcher.find()) {
-            matches.add(new Match.Do(matcher.start()));
-        }
-    }
+    var doMatcher = Pattern.compile("do\\(\\)").matcher(memory);
+    while (doMatcher.find()) matches.add(new Match.Do(doMatcher.start()));
 
-    {
-        var matcher = Pattern.compile("don't\\(\\)").matcher(memory);
-        while (matcher.find()) {
-            matches.add(new Match.Dont(matcher.start()));
-        }
-    }
+    var dontMatcher = Pattern.compile("don't\\(\\)").matcher(memory);
+    while (dontMatcher.find()) matches.add(new Match.Dont(dontMatcher.start()));
 
     matches.sort(Comparator.comparing(Match::idx));
 
@@ -62,14 +62,5 @@ private static String memory() throws IOException {
     return Files.readString(Path.of(path));
 }
 
-sealed interface Match {
-    int idx();
-
-    record Mul(int idx, int product) implements Match {}
-
-    record Do(int idx) implements Match {}
-
-    record Dont(int idx) implements Match {}
-}
 
 
